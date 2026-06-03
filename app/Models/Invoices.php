44090c7711eb95fd2b9c46d9f68ef629991198db
tinwok <tinworknguyen@gmail.com>
2026-06-03 10:50:36 +0800

    Fix merge conflicts

---
+++ app/Models/Invoices.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Invoices extends Model
+{
+    protected $fillable = ['customer_id', 'staff_id', 'payments_id', 'appointment_date', 'start_time', 'end_time', 'note'];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customers::class, 'customer_id');
+    }
+
+    public function staff()
+    {
+        return $this->belongsTo(Staffs::class, 'staff_id');
+    }
+}
